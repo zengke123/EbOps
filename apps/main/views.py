71c1e2750f58b7d70ab81be7aa574e3f38f2411c
zengke123 <249626102@qq.com>
2019-04-27 02:02:57 +0800

    [update] 添加错误页面

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,6 +1,5 @@
 from . import main
 from .. import db
-from ..models import OpsItem
 from flask import render_template
 from flask_login import login_required
 
@@ -8,5 +7,5 @@ from flask_login import login_required
 @main.route("/",methods=['GET','POST'])
 @login_required
 def index():
-    ops_items = db.session.query(OpsItem.t_name, OpsItem.c_name).all()
-    return render_template('index.html', ops_items=ops_items)
+    # ops_items = db.session.query(OpsItem.t_name, OpsItem.c_name).all()
+    return render_template('index.html')
