2e3569a208eb3ee59810d775e6da10f367985c9a
zengke123 <249626102@qq.com>
2019-04-28 22:31:40 +0800

    [update] 调整用户管理

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -6,7 +6,7 @@ from flask import render_template, request, url_for, flash, redirect
 from flask_login import login_user, login_required, logout_user
 
 
-@auth.route("/login",methods=['GET','POST'])
+@auth.route("/login", methods=['GET','POST'])
 def login():
     form = LoginForm()
     # 渲染HTML时需加入 {{ form.csrf_token }} 否则validate_on_submit一直为False
@@ -32,3 +32,36 @@ def logout():
 def users():
     users = User.query.all()
     return render_template("user_list.html", app='用户管理', action='用户列表', users=users)
+
+
+@auth.route('/create', methods=['GET','POST'])
+@login_required
+def create():
+    if request.method == "GET":
+        return render_template('user_create.html', app='用户管理', action='创建用户')
+    else:
+        user = request.form
+        password1 = user.get('password1')
+        password2 = user.get('password2')
+        if password1 != password2:
+            error = "两次密码不一致，请确认.."
+            return render_template('user_create.html', app='用户管理', action='创建用户', error=error)
+        else:
+            role = 1 if user.get('role') == "Admin" else 0
+            status = 1 if user.get('status') == "normal" else 0
+            new_user = {
+                'username': user.get('username'),
+                'password': password1,
+                'role': role,
+                'status': status
+            }
+            add_user = User(**new_user)
+            db.session.add(add_user)
+            db.session.commit()
+            return redirect(url_for('auth.users'))
+
+
+@auth.route('/profile')
+@login_required
+def profile():
+    return render_template('user_profile.html', app='用户信息')
