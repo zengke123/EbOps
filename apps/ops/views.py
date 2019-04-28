24d4b4de102da7527622132576497a65ca69db80
zengke123 <249626102@qq.com>
2019-04-28 16:48:26 +0800

    [test] 增加测试内容

--- apps/ops/views.py
+++ apps/ops/views.py
@@ -1,7 +1,7 @@
 from . import ops
 from .. import db
 from ..models import OpsItem, OpsInfo
-from flask import render_template
+from flask import render_template, request, jsonify
 from flask_login import login_required
 
 
@@ -48,3 +48,30 @@ def cl():
     _ops_infos = db.session.query(OpsInfo.id, OpsInfo.content, OpsInfo.cycle).filter(OpsInfo.t_name == 'cl').all()
     ops_item = _ops_items[0][0]
     return render_template('ops_ims.html', app='作业计划', action=ops_item, ops_item=ops_item, ops_infos=_ops_infos)
+
+
+@ops.route("/log/<log_id>")
+@login_required
+def ops_log(log_id):
+    return render_template('ops_log.html',log_id=log_id)
+
+
+@ops.route("/req")
+@login_required
+def request_log():
+    import os
+    log_id = request.args.get('log_id')
+    filepath = '/Users/EB/PycharmProjects/EbOps'
+    filename = log_id + '.txt'
+    result = ''
+    with open(os.path.join(filepath, filename), encoding="utf-8") as f:
+        lines = f.readlines()
+        for line in lines:
+            result += line + '\r\n'
+        f.close()
+    logs = {
+        "data": result,
+        "end": True,
+        "mark": "7bbfef0c-103d-4b4f-8f43-39e7121db223"
+    }
+    return jsonify(logs)
