60b208a93c7d6fa75cdcb91e61f76057830c6666
zengke123 <249626102@qq.com>
2019-04-28 23:34:18 +0800

    [update] 增加用户信息

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -2,21 +2,30 @@ from . import auth
 from .. import db
 from ..models import User
 from .forms import LoginForm
+from datetime import datetime
 from flask import render_template, request, url_for, flash, redirect
-from flask_login import login_user, login_required, logout_user
+from flask_login import login_user, login_required, logout_user, current_user
 
 
 @auth.route("/login", methods=['GET','POST'])
 def login():
     form = LoginForm()
     # 渲染HTML时需加入 {{ form.csrf_token }} 否则validate_on_submit一直为False
+    error = None
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
+        error = '用户或密码错误'
         if user is not None and user.verify_password(form.password.data):
-            login_user(user, form.remember_me.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
-        flash('Invalid username or password.')
-    return render_template("login.html", form=form)
+            if user.status == 1:
+                login_user(user, form.remember_me.data)
+                login_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                user.last_time = login_time
+                db.session.commit()
+                return redirect(url_for('main.index'))
+                # return redirect(request.args.get('next') or url_for('main.index'))
+            else:
+                error = "该用户已被锁定"
+    return render_template("login.html", form=form, error=error)
 
 
 @auth.route('/logout')
@@ -52,8 +61,10 @@ def create():
             new_user = {
                 'username': user.get('username'),
                 'password': password1,
+                'mail': user.get('email'),
                 'role': role,
-                'status': status
+                'status': status,
+                'create_time': datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             }
             add_user = User(**new_user)
             db.session.add(add_user)
@@ -64,4 +75,5 @@ def create():
 @auth.route('/profile')
 @login_required
 def profile():
-    return render_template('user_profile.html', app='用户信息')
+    user =  User.query.filter(User.username == current_user.username).first()
+    return render_template('user_profile.html', app='用户信息', user=user)
