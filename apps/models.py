f6a3180340af7520095ede21b6f501a05532e760
zengke123 <249626102@qq.com>
2019-04-28 22:08:07 +0800

    [update] 添加创建用户

--- apps/models.py
+++ apps/models.py
@@ -8,7 +8,7 @@ class User(UserMixin, db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    password = db.Column(db.String(64), index=True, unique=True)
+    password = db.Column(db.String(64))
     role = db.Column(db.SmallInteger, default=2)
     status = db.Column(db.SmallInteger, default=0)
 
