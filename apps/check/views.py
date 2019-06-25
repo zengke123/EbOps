9dffeea89f0a9bac1fde128331355e6cc63cdd02
zengke123 <249626102@qq.com>
2019-06-25 10:11:29 +0800

    [update] 增加例检记录日期搜索

--- apps/check/views.py
+++ apps/check/views.py
@@ -15,6 +15,10 @@ def info():
 def history():
     # 获取get请求传过来的页数,没有传参数，默认为1
     page = int(request.args.get('page', 1))
-    paginate = CheckHistory.query.order_by(CheckHistory.id.asc()).paginate(page, per_page=10, error_out=False)
+    date = request.args.get('date')
+    if date:
+        paginate = CheckHistory.query.filter(CheckHistory.checktime.like(date+'%')).order_by(CheckHistory.id.asc()).paginate(page, per_page=10, error_out=False)
+    else:
+        paginate = CheckHistory.query.order_by(CheckHistory.id.asc()).paginate(page, per_page=10, error_out=False)
     datas = paginate.items
-    return render_template('check_history.html', app="自动例检", action="例检记录", paginate=paginate, datas=datas)
+    return render_template('check_history.html', app="自动例检", action="例检记录", paginate=paginate, datas=datas, date=date)
